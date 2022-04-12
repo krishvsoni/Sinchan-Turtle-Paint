11e774bed6aabf4923374c4d0b8514c1838802fb
KRISH SONI <67964054+Techno-Krish@users.noreply.github.com>
2022-04-12 21:13:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-# This is a sample Python script.
+# A CODE BY KRISH SONI
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from turtle import *
 
 import time
@@ -589,3 +587,4 @@ allEyes()
 myRobot()
 ht()
 done()
+
